0c72e5ecf9833905d9064c609516ec64a495d3c5
Ryota <ryota15765@icloud.com>
2022-04-03 15:37:14 +0900

    send signal base

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 Hack infra-red remote controllers at home.
 
+## Setup
+
+```
+sudo pip3 install wiringpi
+```
+
 ## References
 - https://qiita.com/kitahara1152/items/378a36c9e60dca78f80a
 - https://osoyoo.com/2017/07/07/ir-remote/
-
+- https://paltee.net/archives/180
---
+++ send-signal.py
@@ -0,0 +1,56 @@
+import wiringpi
+import time
+
+# setup
+OUTPUT_PIN = 18
+FREQ = 38e3
+RANGE = 3
+CLOCK = int(19.2e6/FREQ/RANGE) 
+T = 370
+
+MICRO = 1000000
+
+def send_zero():
+    wiringpi.pwmWrite(OUTPUT_PIN, 1)
+    time.sleep(T/MICRO)
+    wiringpi.pwmWrite(OUTPUT_PIN, 0)
+    time.sleep(T/MICRO)
+
+def send_one():
+    wiringpi.pwmWrite(OUTPUT_PIN, 1)
+    time.sleep(T/MICRO)
+    wiringpi.pwmWrite(OUTPUT_PIN, 0)
+    time.sleep(3*T/MICRO)
+
+def send_data(data="110010001110100000010100"):
+    wiringpi.pwmWrite(OUTPUT_PIN,1)
+    time.sleep(8*T/MICRO)
+    wiringpi.pwmWrite(OUTPUT_PIN,0)
+    time.sleep(4*T/MICRO)
+
+    for d in data:
+        if d == "0":
+            send_zero()
+        else:
+            send_one()
+
+    wiringpi.pwmWrite(OUTPUT_PIN,0)
+    return
+
+def main():
+    wiringpi.wiringPiSetupGpio()
+    wiringpi.pinMode(OUTPUT_PIN, wiringpi.GPIO.PWM_OUTPUT)
+    wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+    wiringpi.pwmSetRange(RANGE)
+    wiringpi.pwmSetClock(CLOCK)
+
+    time.sleep(1)
+
+    send_data()
+
+    time.sleep(1)
+
+    send_data()
+
+if __name__ == '__main__':
+    main()
