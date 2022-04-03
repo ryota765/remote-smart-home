0104e9564ee8453a2e60871b322290f75ad7f2ef
Ryota <ryota15765@icloud.com>
2022-04-03 12:06:34 +0900

    fix bug

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Hack infra-red remote controllers at home.
 ## References
 - https://qiita.com/kitahara1152/items/378a36c9e60dca78f80a
 - https://osoyoo.com/2017/07/07/ir-remote/
+
--- recieve-remote-controller-signal.py
+++ recieve-remote-controller-signal.py
@@ -4,24 +4,37 @@ import time
 # setup
 INPUT_PIN = 17
 
+# Sensor output:
+#   IR signal    -> 0
+#   No IR signal -> 1
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(INPUT_PIN, GPIO.IN)
 
+MICRO = 1000000
+
 def main():
   prev_pin_value = None
+  L = None
+  H = None
 
   while True:
     pin_value = GPIO.input(INPUT_PIN)
 
     if prev_pin_value is None:
       prev_pin_value = pin_value
-      start_time = time.time()
-      print(f'Start! value: {pin_value}, start_time: {start_time}')
+      last_time = time.time()
+      print(f'Start! value: {pin_value}, start_time: {last_time}')
     elif prev_pin_value != pin_value:
       prev_pin_value = pin_value
-      print(f'Value changed! value: {pin_value}, elapsed_time: {time.time() - start_time}')
+
+      if pin_value == 1: # changed to LOW
+        H = int((time.time() - last_time) * MICRO)
+      else: # changed to HIGH
+        L = int((time.time() - last_time) * MICRO)
+        print(f'H:L = {H}:{L}')
+      last_time = time.time()
     else:
-      time.sleep(50/1000) # 50 millisec
+      time.sleep(50/MICRO) # 50 microsec
 
 if __name__ == '__main__':
   main()
